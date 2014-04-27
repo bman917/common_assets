602e12edba11412815a7dd655226e1c0fde8e4b6
Mr Jacky Chan <bman917@gmail.com>
2014-04-27 14:01:35 +0800

    spinner centering

--- app/assets/javascripts/common_tools.js
+++ app/assets/javascripts/common_tools.js
@@ -8,3 +8,12 @@ function focus_on_next_tab() {
   nexttab = currenttab * 1 + 1;
   $("[tabindex='" + nexttab + "']").focus();
 }
+
+jQuery.fn.center = function () {
+    this.css("position","absolute");
+    this.css("top", Math.max(0, (($(window).height() - $(this).outerHeight()) / 2) + 
+                                                $(window).scrollTop()) + "px");
+    this.css("left", Math.max(0, (($(window).width() - $(this).outerWidth()) / 2) + 
+                                                $(window).scrollLeft()) + "px");
+    return this;
+}
