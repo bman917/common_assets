543ca5bcad4e542934f0143471afa22ca94dada4
Mr Jacky Chan <bman917@gmail.com>
2014-05-02 01:14:17 +0800

    alt and title for edit and delete image. update autocomplete css

--- lib/common_assets/application_helper.rb
+++ lib/common_assets/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
   def delete_img
-    image_tag 'gtk-delete.png', size: '14x14'
+    image_tag 'gtk-delete.png', size: '14x14', alt: 'Delete', title: 'Delete'
   end
 
   def edit_img
-    image_tag 'gtk-edit.png', size: '14x14'
+    image_tag 'gtk-edit.png', size: '14x14', alt: 'Edit', title: 'Edit'
   end
 end
--- lib/common_assets/version.rb
+++ lib/common_assets/version.rb
@@ -1,3 +1,3 @@
 module CommonAssets
-  VERSION = "0.1.1"
+  VERSION = "0.1.2"
 end
