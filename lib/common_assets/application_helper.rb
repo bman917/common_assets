bc6d04294ec8a088682783e0fa77e6f4e19d5c2d
Mr Jacky Chan <bman917@gmail.com>
2014-05-13 02:29:32 +0800

    lock and unlock

--- lib/common_assets/application_helper.rb
+++ lib/common_assets/application_helper.rb
@@ -6,4 +6,20 @@ module ApplicationHelper
   def edit_img
     image_tag 'gtk-edit.png', size: '14x14', alt: 'Edit', title: 'Edit'
   end
+
+  def locked_img(title='Locked', options={size: '14x14'})
+    
+    options[:alt] = title
+    options[:title] = title
+
+  	image_tag 'Crystal_Clear_action_lock2.png', options
+  end
+
+  def unlocked_img(title='Unlocked', options={size: '14x14'})
+    
+    options[:alt] = title
+    options[:title] = title
+
+  	image_tag 'Crystal_Clear_action_half_lock.png', options
+  end
 end
--- lib/common_assets/version.rb
+++ lib/common_assets/version.rb
@@ -1,3 +1,3 @@
 module CommonAssets
-  VERSION = "0.1.2"
+  VERSION = "0.1.3"
 end
