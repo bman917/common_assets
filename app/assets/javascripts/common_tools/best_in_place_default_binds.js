05b7a5e9bc48117f2702aefadf8b384d99a46148
Mr Jacky Chan <bman917@gmail.com>
2014-05-15 00:40:31 +0800

    fix bip binding

--- app/assets/javascripts/common_tools/best_in_place_default_binds.js
+++ app/assets/javascripts/common_tools/best_in_place_default_binds.js
@@ -1,7 +1,12 @@
-$( document ).ready(function() {
-	$('.best_in_place').bind(
-	  "ajax:success", 
-	  function() {
-	  	flash_success("Update Successful");
+function bind_bip_success() {
+	$( document ).ready(function() {
+		$('.best_in_place').bind(
+		  "ajax:success", 
+		  function() {
+		  	flash_success("Update Successful");
+		});
 	});
-});
+}
+
+bind_bip_success();
+document.addEventListener("page:change", bind_bip_success);
